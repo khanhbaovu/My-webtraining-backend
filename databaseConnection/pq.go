bbfdb421e34880b2dab960c018b06a267d3504a5
= <=>
2020-01-06 09:15:34 +0700

    commit databaseConnection

--- databaseConnection/pq.go
+++ databaseConnection/pq.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-  "fmt"
+//  "fmt"
 	"database/sql"
   "log"
 	_ "github.com/lib/pq"
 )
-
+type Tag struct {
+    ID   string    `json:"id"`
+    Name string `json:"name"`
+}
 func main() {
-  connStr := "host=localhost port=5432 user=postgres password=Vbk02122000 dbname=postgres sslmode=disabled"
+  connStr := "host=localhost port=5432 user=postgres password=Vbk02122000 dbname=postgres sslmode=disable"
 
   db, err := sql.Open("postgres", connStr)
 
@@ -16,6 +19,22 @@ func main() {
     log.Fatal(err)
   }
 
-  fmt.Println(db)
+  defer db.Close()
+  db.Query("INSERT INTO students VALUES ('B00977056', 'Nguyen Huu Hai', 8)")
+  results, err := db.Query("SELECT id, name FROM students")
+    if err != nil {
+        panic(err.Error()) // proper error handling instead of panic in your app
+    }
+
+    for results.Next() {
+        var tag Tag
+        // for each row, scan the result into our tag composite object
+        err = results.Scan(&tag.ID, &tag.Name)
+        if err != nil {
+            panic(err.Error()) // proper error handling instead of panic in your app
+        }
+                // and then print out the tag's Name attribute
+        log.Printf(tag.Name)
+    }
 
 }
